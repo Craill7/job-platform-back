688176ee3dc75c2b72461c2781317386c75521b4
ChingMan05 <859766083@qq.com>
2025-11-17 23:59:30 +0800

    修改获取岗位列表接口

--- src/main/java/cn/sysu/sse/recruitment/job_platform_api/server/mapper/LocationMapper.java
+++ src/main/java/cn/sysu/sse/recruitment/job_platform_api/server/mapper/LocationMapper.java
@@ -1,10 +1,12 @@
 package cn.sysu.sse.recruitment.job_platform_api.server.mapper;
 
-import cn.sysu.sse.recruitment.job_platform_api.pojo.entity.Province;
 import cn.sysu.sse.recruitment.job_platform_api.pojo.entity.City;
+import cn.sysu.sse.recruitment.job_platform_api.pojo.entity.Province;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 省市数据 Mapper
@@ -24,4 +26,29 @@ public interface LocationMapper {
      * @return 城市列表
      */
     List<City> findCitiesByProvinceId(Integer provinceId);
+
+    /**
+     * 根据关键字（名称/简称/代码）查找省份
+     * @param keyword 省份关键字
+     * @return 省份
+     */
+    Optional<Province> findProvinceByKeyword(@Param("keyword") String keyword);
+
+    /**
+     * 根据关键字查找城市，可选限定省份
+     * @param keyword 城市关键字
+     * @param provinceId 省份ID，可为空
+     * @return 城市
+     */
+    Optional<City> findCityByKeyword(@Param("keyword") String keyword, @Param("provinceId") Integer provinceId);
+
+    /**
+     * 批量查询省份
+     */
+    List<Province> findProvincesByIds(@Param("ids") List<Integer> ids);
+
+    /**
+     * 批量查询城市
+     */
+    List<City> findCitiesByIds(@Param("ids") List<Integer> ids);
 }
