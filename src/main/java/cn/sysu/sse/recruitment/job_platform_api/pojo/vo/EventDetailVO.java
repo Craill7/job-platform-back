5ee9be76f50896360310a02bbfe12109e1951839
LinYuan201712 <1254580337@qq.com>
2025-11-25 13:09:17 +0800

    学生端新增接口 1. 获取招聘活动详情公告 2. 获取全部招聘活动列表 (带筛选) 数据库events表字段修改

---
+++ src/main/java/cn/sysu/sse/recruitment/job_platform_api/pojo/vo/EventDetailVO.java
@@ -0,0 +1,31 @@
+package cn.sysu.sse.recruitment.job_platform_api.pojo.vo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class EventDetailVO {
+    @JsonProperty("event_id")
+    private Long eventId;
+
+    @JsonProperty("event_title")
+    private String eventTitle;
+
+    @JsonProperty("event_start_time")
+    private String eventStartTime; // 格式化后的时间字符串
+
+    @JsonProperty("event_end_time")
+    private String eventEndTime;
+
+    @JsonProperty("event_location")
+    private String eventLocation;
+
+    @JsonProperty("event_type")
+    private String eventType;
+
+    @JsonProperty("event_target_audience")
+    private String eventTargetAudience; // 转换后的中文描述
+
+    @JsonProperty("event_summary")
+    private String eventSummary;
+}
---
+++ src/main/java/cn/sysu/sse/recruitment/job_platform_api/pojo/vo/EventListResponseVO.java
@@ -0,0 +1,30 @@
+package cn.sysu.sse.recruitment.job_platform_api.pojo.vo;
+
+import cn.sysu.sse.recruitment.job_platform_api.common.result.Pagination;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class EventListResponseVO {
+    private List<EventItem> events;
+    private Pagination pagination;
+
+    @Data
+    public static class EventItem {
+        @JsonProperty("event_id")
+        private Long eventId;
+
+        @JsonProperty("event_title")
+        private String eventTitle;
+
+        @JsonProperty("event_start_time")
+        private String eventStartTime;
+
+        @JsonProperty("event_location")
+        private String eventLocation;
+
+        @JsonProperty("event_summary")
+        private String eventSummary;
+    }
+}
